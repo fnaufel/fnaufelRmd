fb10f9601eecb4e2e48b72b236bbf320f7218da3
Fernando Naufel <fnaufel@gmail.com>
2021-12-03 12:39:51 -0300

    Start changes

---
+++ R/bs4_book.R
@@ -0,0 +1,102 @@
+#' Custom HTML template
+#'
+#' Loads additional style and configs
+#'
+#' @title bs4_book
+#'
+#' @param theme see [bookdown::bs4_book]
+#' @param template see [bookdown::bs4_book]
+#' @param includes see [bookdown::bs4_book]
+#' @param css see [bookdown::bs4_book]
+#' @param repo see [bookdown::bs4_book]
+#' @param site see [bookdown::bs4_book]
+#' @param lib_dir see [bookdown::bs4_book]
+#' @param pandoc_args see [bookdown::bs4_book]
+#' @param extra_dependencies see [bookdown::bs4_book]
+#' @param split_bib see [bookdown::bs4_book]
+#' @param section_divs see [bookdown::bs4_book]
+#' @param fig_width see [bookdown::bs4_book]
+#' @param fig_height see [bookdown::bs4_book]
+#' @param fig_retina see [bookdown::bs4_book]
+#' @param fig_caption see [bookdown::bs4_book]
+#' @param dev see [bookdown::bs4_book]
+#' @param df_print see [bookdown::bs4_book]
+#' @param mathjax see [bookdown::bs4_book]
+#' @param keep_md see [bookdown::bs4_book]
+#' @param md_extensions see [bookdown::bs4_book]
+#' @param ... see [bookdown::bs4_book]
+#'
+#' @return see [bookdown::bs4_book]
+#'
+#' @author fnaufel
+#' @export
+#'
+#' @importFrom bookdown bs4_book_theme bookdown_site bs4_book
+#' @importFrom bslib font_google
+#' @importFrom rmarkdown includes
+bs4_book <- function(
+  theme = bookdown::bs4_book_theme(
+    primary = "#086a6a",
+    base_font = bslib::font_google("Lexend"),
+    heading_font = bslib::font_google("Roboto"),
+    code_font = bslib::font_google("JetBrains Mono"),
+  ),
+  template = system.file(
+    "rmarkdown/resources/html/bs4_book_ptbr.html",
+    package = "fnaufelRmd"
+  ),
+  includes = rmarkdown::includes(
+    in_header = system.file(
+      "rmarkdown/resources/html/preamble.html",
+      package = "fnaufelRmd"
+    )
+  ),
+  css = system.file(
+    "rmarkdown/resources/html/styles.css",
+    package = "fnaufelRmd"
+  ),
+  repo = NULL,
+  site = bookdown::bookdown_site,
+  lib_dir = "libs",
+  pandoc_args = NULL,
+  extra_dependencies = NULL,
+  split_bib = FALSE,
+  section_divs = TRUE,
+  fig_width = 7,
+  fig_height = 5,
+  fig_retina = 2,
+  fig_caption = TRUE,
+  dev = 'png',
+  df_print = 'paged',
+  mathjax = 'default',
+  keep_md = FALSE,
+  md_extensions = NULL,
+  ...
+) {
+
+  # call the base function
+  bookdown::bs4_book(
+    theme = theme,
+    repo = repo,
+    site = site,
+    lib_dir = lib_dir,
+    pandoc_args = pandoc_args,
+    extra_dependencies = extra_dependencies,
+    template = template,
+    split_bib = split_bib,
+    section_divs = section_divs,
+    fig_width = fig_width,
+    fig_height = fig_height,
+    fig_retina = fig_retina,
+    fig_caption = fig_caption,
+    dev = dev,
+    df_print = df_print,
+    mathjax = mathjax,
+    css = css,
+    includes = includes,
+    keep_md = keep_md,
+    md_extensions = md_extensions,
+    ...
+  )
+
+}
---
+++ R/pdf_book.R
@@ -0,0 +1,108 @@
+#' Custom PDF template
+#'
+#' Loads additional style and configs
+#'
+#' @title pdf_book
+#'
+#' @param toc see [bookdown::pdf_book]
+#' @param number_sections see [bookdown::pdf_book]
+#' @param fig_caption see [bookdown::pdf_book]
+#' @param pandoc_args see [bookdown::pdf_book]
+#' @param base_format see [bookdown::pdf_book]
+#' @param toc_unnumbered see [bookdown::pdf_book]
+#' @param toc_appendix see [bookdown::pdf_book]
+#' @param toc_bib see [bookdown::pdf_book]
+#' @param quote_footer see [bookdown::pdf_book]
+#' @param highlight_bw see [bookdown::pdf_book]
+#' @param df_print see [bookdown::pdf_book]
+#' @param keep_tex see [bookdown::pdf_book]
+#' @param latex_engine see [bookdown::pdf_book]
+#' @param citation_package see [bookdown::pdf_book]
+#' @param includes see [bookdown::pdf_book]
+#' @param md_extensions see [bookdown::pdf_book]
+#' @param template see [bookdown::pdf_book]
+#' @param ... see [bookdown::pdf_book]
+#'
+#' @return see [bookdown::pdf_book]
+#'
+#' @importFrom rmarkdown pdf_document
+#' @importFrom bookdown pdf_book
+#'
+#' @author fnaufel
+#'
+#' @export
+#'
+pdf_book <- function(
+  toc = TRUE,
+  number_sections = TRUE,
+  fig_caption = TRUE,
+  pandoc_args = c(
+    '--variable=link-as-notes',
+    paste0(
+      '--variable=',
+      'resources:',
+      system.file('rmarkdown/resources', package = 'fnaufelRmd')
+    ),
+    '--variable=subparagraph',
+    paste0(
+      '--filter=',
+      system.file(
+        "rmarkdown/resources/pandoc_filters/dollar.py3",
+        package = "fnaufelRmd"
+      )
+    ),
+    paste0(
+      '--filter=',
+      system.file(
+        "rmarkdown/resources/pandoc_filters/hl.py3",
+        package = "fnaufelRmd"
+      )
+    )
+  ),
+  base_format = rmarkdown::pdf_document,
+  toc_unnumbered = TRUE,
+  toc_appendix = TRUE,
+  toc_bib = TRUE,
+  quote_footer = NULL,
+  highlight_bw = FALSE,
+  df_print = 'tibble',
+  keep_tex = TRUE,
+  latex_engine = 'xelatex',
+  citation_package = 'natbib',
+  includes = rmarkdown::includes(
+    in_header = system.file(
+      "rmarkdown/resources/tex/00-preambulo.tex",
+      package = "fnaufelRmd"
+    )
+  ),
+  md_extensions = '+fenced_divs+bracketed_spans',
+  template = system.file(
+    'rmarkdown/resources/tex/bd.format.latex',
+    package = 'fnaufelRmd'
+  ),
+  ...
+) {
+
+  # call the base function
+  bookdown::pdf_book(
+    toc = toc,
+    number_sections = number_sections,
+    fig_caption = fig_caption,
+    pandoc_args = pandoc_args,
+    base_format = base_format,
+    toc_unnumbered = toc_unnumbered,
+    toc_appendix = toc_appendix,
+    toc_bib = toc_bib,
+    quote_footer = quote_footer,
+    highlight_bw = highlight_bw,
+    df_print = df_print,
+    keep_tex = keep_tex,
+    latex_engine = latex_engine,
+    citation_package = citation_package,
+    includes = includes,
+    md_extensions = md_extensions,
+    template = template,
+    ...
+  )
+
+}
