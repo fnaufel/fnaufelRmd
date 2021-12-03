db1a8bf00703a01ec57c9fe1c6ea282cffed7e30
Fernando Naufel <fnaufel@gmail.com>
2021-12-03 18:31:23 -0300

    Code first version of html_report

---
+++ R/html_report.R
@@ -0,0 +1,105 @@
+#' Custom HTML template
+#'
+#' Loads additional style and configs
+#'
+#' @title html_report
+#'
+#' @param toc see [rmarkdown::html_document]
+#' @param toc see [rmarkdown::html_document]
+#' @param toc see [rmarkdown::html_document]
+#' @param number see [rmarkdown::html_document]
+#' @param anchor see [rmarkdown::html_document]
+#' @param section see [rmarkdown::html_document]
+#' @param fig see [rmarkdown::html_document]
+#' @param fig see [rmarkdown::html_document]
+#' @param fig see [rmarkdown::html_document]
+#' @param fig see [rmarkdown::html_document]
+#' @param dev see [rmarkdown::html_document]
+#' @param df see [rmarkdown::html_document]
+#' @param code see [rmarkdown::html_document]
+#' @param code see [rmarkdown::html_document]
+#' @param self see [rmarkdown::html_document]
+#' @param theme see [rmarkdown::html_document]
+#' @param highlight see [rmarkdown::html_document]
+#' @param mathjax see [rmarkdown::html_document]
+#' @param template see [rmarkdown::html_document]
+#' @param extra see [rmarkdown::html_document]
+#' @param css see [rmarkdown::html_document]
+#' @param includes see [rmarkdown::html_document]
+#' @param keep see [rmarkdown::html_document]
+#' @param lib see [rmarkdown::html_document]
+#' @param md see [rmarkdown::html_document]
+#' @param pandoc see [rmarkdown::html_document]
+#' @param ... see [rmarkdown::html_document]
+#'
+#' @return see [rmarkdown::html_document]
+#'
+#' @author fnaufel
+#' @export
+#' @importFrom bslib bs_theme font_google
+#' @importFrom rmarkdown includes html_document
+#'
+html_report <- function(
+  toc = TRUE,
+  toc_depth = 3,
+  toc_float = FALSE,
+  number_sections = TRUE,
+  anchor_sections = TRUE,
+  section_divs = TRUE,
+  fig_width = 7,
+  fig_height = 5,
+  fig_retina = 2,
+  fig_caption = TRUE,
+  dev = "png",
+  df_print = "paged",
+  code_folding = "show",
+  code_download = TRUE,
+  self_contained = TRUE,
+  theme = "default",
+  highlight = "default",
+  mathjax = "default",
+  template = "default",
+  extra_dependencies = NULL,
+  css = 'html/styles.css',
+  includes = rmarkdown::includes(
+    in_header = 'html/preamble.html'
+  ),
+  keep_md = FALSE,
+  lib_dir = "libs",
+  md_extensions = NULL,
+  pandoc_args = NULL,
+  ...
+) {
+
+  # call the base function
+  rmarkdown::html_document(
+    toc = toc,
+    toc_depth = toc_depth,
+    toc_float = toc_float,
+    number_sections = number_sections,
+    anchor_sections = anchor_sections,
+    section_divs = section_divs,
+    fig_width = fig_width,
+    fig_height = fig_height,
+    fig_retina = fig_retina,
+    fig_caption = fig_caption,
+    dev = dev,
+    df_print = df_print,
+    code_folding = code_folding,
+    code_download = code_download,
+    self_contained = self_contained,
+    theme = theme,
+    highlight = highlight,
+    mathjax = mathjax,
+    template = template,
+    extra_dependencies = extra_dependencies,
+    css = css,
+    includes = includes,
+    keep_md = keep_md,
+    lib_dir = lib_dir,
+    md_extensions = md_extensions,
+    pandoc_args = pandoc_args,
+    ...
+  )
+
+}
